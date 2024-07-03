5678492ebe588d001eb5f994a8603d79ef3e11f4
{ Walyson } <walysonmoura222@gmail.com>
2024-07-03 20:57:17 +0000

    feat: add configs tests

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,16 @@ declare global {
       NODE_ENV: string
       DATABASE_CLIENT: string
       DATABASE_URL: string
+      JWT_PRIVATE_KEY: string
+      JWT_PUBLIC_KEY: string
+      CLOUDFLARE_ACCOUNT_ID: string
+      AWS_BUCKET_NAME: string
+      AWS_ACCESS_KEY_ID: string
+      AWS_SECRET_ACCESS_KEY: string
+      REDIS_HOST: string
+      REDIS_PORT: string
+      REDIS_DB: string
+      PORT: string
     }
   }
 }
